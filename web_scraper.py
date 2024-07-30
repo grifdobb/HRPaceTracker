f7fcd1a68470663cd66b0edac5486c78c5b26501
Griffin Dobbins <griffindobbins@Griffins-MacBook-Pro.local>
2024-07-29 23:21:32 -0400

    Judge tracker

--- web_scraper.py
+++ web_scraper.py
@@ -8,6 +8,15 @@ soup = BeautifulSoup(page.content, 'html.parser')
 yearlystats = soup.find('div','player-stats-yearbyyear')
 hr = int(yearlystats.find('td', class_='col-2 row-0').find('a').text)
 
-# Extract the text inside the <a> tag
+soup = BeautifulSoup(requests.get("https://www.mlb.com/yankees/stats/team").content, 'html.parser')
 
-print(hr)
+table = soup.find('div', class_='stats-body-table team')
+rows = table.find('tbody', class_='notranslate').find_all('tr')
+
+gms = 1
+
+for row in rows:
+    if row.find('a', {'aria-label': 'New York Yankees'}):
+        gms = int(row.find_all('td')[1].text)
+
+print("Aaron Judge is on pace to hit", hr * 162 / gms, "home runs over a 162 game season")
