9c96e27472f06072eda13d06a956a93a8eaf1c6e
Griffin Dobbins <griffindobbins@Griffins-MacBook-Pro.local>
2024-07-29 22:11:26 -0400

    homerun scraper

---
+++ web_scraper.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+URL = "https://www.mlb.com/player/aaron-judge-592450"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+yearlystats = soup.find('div','player-stats-yearbyyear')
+hr = int(yearlystats.find('td', class_='col-2 row-0').find('a').text)
+
+# Extract the text inside the <a> tag
+
+print(hr)
